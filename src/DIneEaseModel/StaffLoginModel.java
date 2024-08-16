c19ebaecb28a1af717327515ed235e81bb54b3fc
Siddhartha <siddhulama555@gmail.com>
2024-08-16 08:09:20 +0545

    changes in StaffLoginModel

--- src/DIneEaseModel/StaffLoginModel.java
+++ src/DIneEaseModel/StaffLoginModel.java
@@ -1,24 +1,19 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package DIneEaseModel;
 
-/**
- *
- * @author roshankhadka
- */
+import DineEaseDatabase.StaffLoginDAO;
+
 public class StaffLoginModel {
-    private final String validUsername = "Staff";
-    private final String validPassword = "Password";
-    private final String validPhoneNumber = "1234567890";
+    private StaffLoginDAO staffLoginDAO;
+
+    public StaffLoginModel() {
+        this.staffLoginDAO = new StaffLoginDAO();
+    }
 
     public boolean validateLogin(String username, String password) {
-        return validUsername.equals(username) && validPassword.equals(password);
+        return staffLoginDAO.validateLogin(username, password);
     }
 
     public boolean resetPassword(String username, String phoneNumber, String newPassword) {
-        return validUsername.equals(username) && validPhoneNumber.equals(phoneNumber);
+        return staffLoginDAO.resetPassword(username, phoneNumber, newPassword);
     }
 }
-    
